58f1e44c54dc03056692ca35bf8b54763106eed2
mtan <mtan@c99d82c2-4e0d-4e5d-879a-efdf01d99ca8>
2012-02-17 14:39:35 +0000

    Fix issue to exclude initmigr
    git-svn-id: https://subversion.ip-connect.de/repositories/Loewenbraeu/net/MobileApp/branches/dev@106 c99d82c2-4e0d-4e5d-879a-efdf01d99ca8

--- src/SolutionItems/AssemblyInfo.cs
+++ src/SolutionItems/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 //      Revision
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyInformationalVersion("1.3.2-rc2")]
-[assembly: AssemblyFileVersion("1.3.20217.0")]
+// [assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.3-rc2")]
+[assembly: AssemblyFileVersion("1.3.20217.1")]
